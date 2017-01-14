3aaae03fc7c78df6f235ea3c8018dfb9d0eeef91
BallWarapol <xxx123xcv@gmail.com>
2017-01-14 11:31:51 +0700

    change to space link in every <p>

--- gen.py
+++ gen.py
@@ -2,6 +2,7 @@
 import glob
 import subprocess
 import shutil
+import re
 
 p="./docs/"
 shutil.rmtree(p)
@@ -11,15 +12,21 @@ for fn in glob.glob(p+"*.htm"):
 	c+=1
 	with open(fn, "r+") as f:
 		data = f.read()
-		data=data.replace("<body>","""<body>source: <a href="http://www.palikanon.com/english/sangaha/sangaha.htm">http://www.palikanon.com/english/sangaha/sangaha.htm</a><script type="text/javascript" id="cool_find_script" src="find6.js"></script><style>.highlight{	background-color: blue;}.find_selected{	background-color: green;}</style>""")
+		data=data.replace("<body>","""<body>source: <a href="http://www.palikanon.com/english/sangaha/sangaha.htm">http://www.palikanon.com/english/sangaha/sangaha.htm</a><script type="text/javascript" id="cool_find_script" src="find6.js"></script><style>.highlight{	background-color: blue;}.find_selected{	background-color: green;} a {color: blue;text-decoration: none; /* no underline */}</style>""")
 		data=data.replace("../../","")
 		data=data.replace("../images/","")
-		data=data.split("<hr>")
+		data=data.split("<p>")
 		for i,v in enumerate(data):
-			data[i]="%s<a name='%s-%s' href='?#%s-%s'><img src=lnk.png /></a>"%(v,c,i,c,i)
+			data[i]="<a name='%s-%s' href='?#%s-%s'>&nbsp;</a>"%(c,i,c,i)+data[i]
 		f.seek(0)
-		f.write("<hr>".join(data))
-		f.truncate()
+		f.write("<p>".join(data))
+		f.truncate()		
+		#data=data.split("<hr>")
+		#for i,v in enumerate(data):
+		#	data[i]="%s<a name='%s-%s' href='?#%s-%s'><img src=lnk.png /></a>"%(v,c,i,c,i)
+		#f.seek(0)
+		#f.write("<hr>".join(data))
+		#f.truncate()
 combFnP=p+"index.htm"
 try:
 	shutil.rmtree(combFnP)
